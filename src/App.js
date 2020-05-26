1777e5bc0329ff8755cccc5c176b520b2ad6978b
Sam Barrick <you@example.com>
2020-05-26 18:04:28 -0500

    auth0 attempt number 2. Basic login button up top redirecting to auth0 global sign in. NOTHING else though.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import "./App.css";
 import Navigation from "./components/Navigation";
 //import NavigationLoggedIn from './components/NavigationLoggedIn'
@@ -7,15 +7,25 @@ import { Provider } from "react-redux";
 import Router from "./Router";
 import store from "./redux/store";
 import "./App.css";
-
+import { Auth0Context } from './contexts/auth0-context'; 
 
 function App() {
- 
+  const auth0 = useContext(Auth0Context);
 
   return (
   
     <Provider store={store}>
       <BrowserRouter>
+      <div className="hero is-info is-fullheight">
+      <div className="hero-body">
+        <div className="container has-text-centered">
+          <h1>Click Below!</h1>
+          <button onClick={auth0.loginWithRedirect} className="button is-danger">
+            Login
+          </button>
+        </div>
+      </div>
+    </div>
         <Navigation />
         <Router />
       </BrowserRouter>
